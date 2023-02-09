5e43a2146715ca7e8e885f9d4cdc61a414f02df7
Sousettia <104794480+Sousettia@users.noreply.github.com>
2023-02-09 13:56:07 +0700

    Week11 Example

---
+++ JavaProgram/Java_Week11/src/RectangleDemo2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class RectangleDemo2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input width : ");
+        double width = scan.nextDouble();
+        System.out.print("Input length : ");
+        double length = scan.nextDouble();
+
+        Rectangle2 rec = new Rectangle2(width, length);
+        System.out.println(rec + "color = " + Shape2.color + "\nArea of Rectangle is " + rec.getArea());
+    }
+
+}
---
+++ JavaProgram/Java_Week11/src/Shape.java
@@ -0,0 +1,19 @@
+public abstract class Shape {
+    
+    private String color;
+
+    public Shape(String color){
+        this.color = color;
+    }
+    public Shape(){
+        color = null;
+    }
+
+    //abstract method
+    public abstract double getArea();
+
+    //return String in form "color = [color]"
+    public String toString(){
+        return "color = " + this.color;
+    }
+}
