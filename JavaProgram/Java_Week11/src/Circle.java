6ed8d09270aed593028981cc5c1ab26b85d4e05c
Sousettia <104794480+Sousettia@users.noreply.github.com>
2023-02-09 14:22:09 +0700

    Practice 1 [09-2-2023]

---
+++ JavaProgram/Java_Week11/src/Circle.java
@@ -0,0 +1,20 @@
+public class Circle extends Shape {
+
+    private double radius;
+    
+    public Circle(double radius,String color) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+    public String toString(){
+        return "radius = "+this.radius;
+    }
+}
